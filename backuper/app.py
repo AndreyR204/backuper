b7ffd80bdd902b4a484807c0991a06282a6d6f9f
AndreyR204 <andrejrybenok@gmail.com>
2020-06-02 16:51:03 +0500

    Create subprocess

--- backuper/app.py
+++ backuper/app.py
@@ -1,23 +1,68 @@
 import configparser
-from backuper import launcher
+from pathlib import Path
+from subprocess import Popen
 
 
 def config_changer(args):
     config = configparser.ConfigParser()
-    if args.set1:
+    config2 = configparser.ConfigParser()
+    if not Path('conf.ini').is_file():
+        proc = Popen("python backuper/backuper.pyw")
         address1 = input('Enter FTP server address:')
         port1 = input('Enter FTP server port: ')
         login1 = input('Enter login for FTP: ')
         password1 = input('Enter password for FTP: ')
-        config['FTP'] = {'login1': str(login1), 'password1': str(password1), 'address1': str(address1), 'port1': str(port1),
-                         'time1': str(args.time1), 'list1': str(args.list1)}
-    if args.set2:
+        config['FTP'] = {'login1': str(login1), 'password1': str(password1), 'address1': str(address1),
+                         'port1': str(port1)}
         login2 = input('Enter login for 2: ')
         password2 = input('Enter password for 2: ')
-        config['2'] = {'login2': login2, 'password2': password2, 'time2': args.time2, 'list2': args.list2}
+        config['2'] = {'login2': str(login2), 'password2': str(password2)}
+        config['Times'] = {'time1': input('Enter time for FTP: ')}
+        config['Times']['time2'] = input('Enter time for 2: ')
+        config['Lists'] = {'list1': input('Enter list for FTP: ')}
+        config['Lists']['list2'] = input('Enter list for 2: ')
+        config['Process'] = {'stop': str(False)}
+    else:
+        if args.set1:
+            address1 = input('Enter FTP server address:')
+            port1 = input('Enter FTP server port: ')
+            login1 = input('Enter login for FTP: ')
+            password1 = input('Enter password for FTP: ')
+            config['FTP'] = {'login1': str(login1), 'password1': str(password1), 'address1': str(address1),
+                             'port1': str(port1)}
+        else:
+            config2.read('conf.ini')
+            config['FTP'] = config2['FTP']
+        if args.set2:
+            login2 = input('Enter login for 2: ')
+            password2 = input('Enter password for 2: ')
+            config['2'] = {'login2': str(login2), 'password2': str(password2)}
+        else:
+            config2.read('conf.ini')
+            config['2'] = config2['2']
+        if args.time1 != 0:
+            config['Times'] = {'time1': str(args.time1)}
+        else:
+            config2.read('conf.ini')
+            config['Times'] = {'time1': config2['Times']['time1']}
+        if args.time2 != 0:
+            config['Times'] = {'time2': str(args.time2)}
+        else:
+            config2.read('conf.ini')
+            config['Times'] = {'time2': config2['Times']['time2']}
+        config['Process'] = {'stop': str(args.stop)}
+        if args.list1 is not None:
+            config['Lists'] = {'list1': str(args.list1)}
+        else:
+            config2.read('conf.ini')
+            config['Lists'] = {'list1': config2['Lists']['list1']}
+        if args.list2 is not None:
+            config['Lists'] = {'list2': str(args.list2)}
+        else:
+            config2.read('conf.ini')
+            config['Lists'] = {'list2': config2['Lists']['list2']}
     with open('conf.ini', 'w') as configfile:
         config.write(configfile)
-    if args.stop:
-        launcher.process_executter(True)
-    else:
-        launcher.process_executter(False)
+    config.read('conf.ini')
+    if config['Process']['stop'] == 'True':
+        proc = Popen("python backuper/backuper.pyw")
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import argparse
-from config import config
 from backuper import app
 
 parser = argparse.ArgumentParser()
@@ -8,8 +7,8 @@ parser.add_argument('--set1', help='authorisation for FTP', action='store_true')
 parser.add_argument('--set2', help='authorisation for 2', action='store_true')
 parser.add_argument('--list1', '-l1', default=None, help='txt file with listed directories to backup on FTP')
 parser.add_argument('--list2', '-l2', default=None, help='txt file with listed directories to backup on 2')
-parser.add_argument('--time1', '-t1', default=config.time1, help='period of backuping in minutes')
-parser.add_argument('--time2', '-t2', default=config.time2, help='period of backuping in minutes')
+parser.add_argument('--time1', '-t1', default=0, help='period of backuping in minutes')
+parser.add_argument('--time2', '-t2', default=0, help='period of backuping in minutes')
 parser.add_argument('--stop', help='stop backuping', action='store_true')
 parser.add_argument('--status', help='status of process')
 args = parser.parse_args()
